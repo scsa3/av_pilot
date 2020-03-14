3748de501c6973eadfb2095d9c08da2c8c3aa7f0
Wei-Han He <scsa32@gmail.com>
2020-03-14 18:29:16 +0800

    Init

--- category/models.py
+++ category/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+from movies.models import Movie
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+    movies = models.ManyToManyField(Movie)
